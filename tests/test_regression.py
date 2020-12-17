4a2a4f708b9f99e23bc0afe43732b6256aa1a152
bobturneruk <r.d.turner@sheffield.ac.uk>
2020-12-17 12:38:59 +0000

    update regression test for new achitecture

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -5,7 +5,12 @@ def test_matching_baseline():
     '''Test that output of the major function in the module is unchanged.'''
     masses_file_name = "data/E_coli_disaccharides_monomers_only.csv"
     ftrs_file_name = "data/OT_200124_Ecoli_WT_1_Rep1.ftrs"
-    matching.main(ftrs_file_name, masses_file_name)
+
+    raw_data = matching.ftrs_reader(ftrs_file_name)
+    theo_masses = matching.theo_masses_reader(masses_file_name)
+    mod_test = ['Sodium','Nude', 'DeAc']
+
+    results = matching.data_analysis(raw_data, theo_masses, 0.5, mod_test)
     
     baseline_df = pd.read_csv("data/baseline_output.csv")
     output_df = pd.read_csv("output.csv")
