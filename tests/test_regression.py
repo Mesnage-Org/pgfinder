3d36d4043dfe128407c8576a188d168174bbd085
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-02-14 09:38:55 +0000

    fix mod_test in tests

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -8,7 +8,7 @@ def test_matching_baseline():
 
     raw_data = matching.ftrs_reader(ftrs_file_name)
     theo_masses = matching.theo_masses_reader(masses_file_name)
-    mod_test = ['Sodium','Nude', 'DeAc']
+    mod_test = ['Sodium','Potassium','Anhydro','DeAc','Deacetyl_Anhydro','Nude','Decay','Amidation','Amidase','Double_Anh','Multimers']
 
     output_df = matching.data_analysis(raw_data, theo_masses, 0.5, mod_test, 10)
     
