6e61b16262080319379fd865032d7c109b48edac
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-02-14 09:29:31 +0000

    @Ankur-AVP-Patel proposals from #27

--- make_baseline.py
+++ make_baseline.py
@@ -5,7 +5,7 @@ ftrs_filepath = "data/test_ms_data.ftrs"
 
 raw_data = matching.ftrs_reader(ftrs_filepath)
 theo_masses = matching.theo_masses_reader(csv_filepath)
-mod_test = ['Sodium','Nude', 'DeAc']
+mod_test = ['Sodium','Potassium','Anhydro','DeAc','Deacetyl_Anhydro','Nude','Decay','Amidation','Amidase','Double_Anh','Multimers']
 results = matching.data_analysis(raw_data, theo_masses, 0.5, mod_test)
 
 results.to_csv("data/baseline_output.csv")
