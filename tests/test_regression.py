f44e94869d971679e82d5c317bd93f724acb8a5c
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-02-14 18:32:25 +0000

    fix copy paste errors

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import ms1.matching as matching
+import ms1.validation as validation
 
 def test_matching_baseline():
     '''Test that output of the major function in the module is unchanged.'''
     masses_file_name = "data/test_masses.csv"
     ftrs_file_name = "data/test_ms_data.ftrs"
 
-    raw_data = matching.ftrs_reader(ftrs_filepath)
+    raw_data = matching.ftrs_reader(ftrs_file_name)
     validation.validate_raw_data_df(raw_data)
 
-    theo_masses = matching.theo_masses_reader(csv_filepath)
+    theo_masses = matching.theo_masses_reader(masses_file_name)
     validation.validate_theo_masses_df(theo_masses)
 
     mod_test = ['Sodium','Potassium','Anhydro','DeAc','Deacetyl_Anhydro','Nude','Decay','Amidation','Amidase','Double_Anh','Multimers']
