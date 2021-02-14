f3c92be2528394c23cb54993efdad934c5a4604f
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-02-14 09:36:25 +0000

    set user_ppm 10

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -10,7 +10,7 @@ def test_matching_baseline():
     theo_masses = matching.theo_masses_reader(masses_file_name)
     mod_test = ['Sodium','Nude', 'DeAc']
 
-    output_df = matching.data_analysis(raw_data, theo_masses, 0.5, mod_test)
+    output_df = matching.data_analysis(raw_data, theo_masses, 0.5, mod_test, 10)
     
     baseline_df = pd.read_csv("data/baseline_output.csv", index_col=0)
 
