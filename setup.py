c4ec911c91ecf88918c0055cf099519b1d40fb84
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-05-11 10:33:05 +0100

    split binder requirements from `setup.py`

--- setup.py
+++ setup.py
@@ -6,11 +6,7 @@ setup(
     url='https://github.com/Mesnage-Org/pgfinder',
     author='pgfinder Team',
     packages=find_packages(),    
-    install_requires=['hide_code>=0.6.0',
-                      'ipysheet>=0.4.4',
-                      'ipywidgets>=7.6.3',
-                      'jupyterlab>=3.0.15',
-                      'numpy>=1.20.3', 
+    install_requires=['numpy>=1.20.3', 
                       'pandas>=1.2.4', 
                       'pysqlite3'],
 )
