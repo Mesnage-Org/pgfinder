e7acc5ad55795ac9c7b33f18798b6adeffb54872
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-05-11 11:14:31 +0100

    whitespace

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='0.0.1',
     url='https://github.com/Mesnage-Org/pgfinder',
     author='pgfinder Team',
-    packages=find_packages(),    
-    install_requires=['numpy>=1.20.3', 
-                      'pandas>=1.2.4', 
+    packages=find_packages(),
+    install_requires=['numpy>=1.20.3',
+                      'pandas>=1.2.4',
                       'pysqlite3'],
 )
