b8918a5a2a591a353349cddd886c588642f5561f
bobturneruk <r.d.turner@sheffield.ac.uk>
2022-01-25 20:44:42 +0000

    match interface and metadata save

--- demo.py
+++ demo.py
@@ -1,18 +1,14 @@
 import pgfinder.matching as matching
 import pgfinder.validation as validation
 
-csv_filepath = "data/masses/e_coli_monomer_masses.csv"
 mq_filepath = "data/maxquant_test_data.txt"
-
-raw_data = matching.maxquant_file_reader(mq_filepath)
-validation.validate_raw_data_df(raw_data)
-
-theo_masses = matching.theo_masses_reader(csv_filepath)
-validation.validate_theo_masses_df(theo_masses)
+csv_filepath = "data/masses/e_coli_monomer_masses.csv"
 
 mod_test = ['Sodium','Potassium','Anhydro','DeAc','Deacetyl_Anhydro','Nude','Decay','Amidation','Amidase','Double_Anh','multimers_Glyco']
-validation.validate_enabled_mod_list(mod_test)
 
-results = matching.data_analysis(raw_data, theo_masses, 0.5, mod_test, 10)
+results = matching.match(mq_filepath, csv_filepath, 0.5, mod_test, 10)
 
+print(results.attrs['metadata'])
 print(results)
+
+matching.dataframe_to_csv_metadata(save_filepath='./', output_dataframe=results)
