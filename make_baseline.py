8c3f9bb9c39e903da455e980779183b7469e110b
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-02-02 11:48:17 +0000

    include metadata in baseline

--- make_baseline.py
+++ make_baseline.py
@@ -7,13 +7,12 @@ theo_masses = pgio.theo_masses_reader(csv_filepath)
 mod_test = ['Sodium','Potassium','Anhydro','DeAc','Deacetyl_Anhydro','Nude','Decay','Amidation','Amidase','Double_Anh','multimers_Glyco']
 
 # Generate maxquant baseline
-mq_file_name = "data/maxquant_test_data.txt"
-raw_data_mq = pgio.maxquant_file_reader(mq_file_name)
-results = matching.data_analysis(raw_data_mq, theo_masses, 0.5, mod_test, 10)
-results.to_csv("data/baseline_output_mq.csv")
+mq_filepath = "data/maxquant_test_data.txt"
+results = matching.match(mq_filepath, csv_filepath, 0.5, mod_test, 10)
+pgio.dataframe_to_csv_metadata(save_filepath='./data/', output_dataframe=results, filename='baseline_output_mq.csv')
+
 
 # Generate ftrs baseline
-ftrs_file_name = "data/ftrs_test_data.ftrs"
-raw_data_ftrs = pgio.ftrs_reader(ftrs_file_name)
-results = matching.data_analysis(raw_data_ftrs, theo_masses, 0.5, mod_test, 10)
-results.to_csv("data/baseline_output_ftrs.csv")
+ftrs_filepath = "data/ftrs_test_data.ftrs"
+results = matching.match(ftrs_filepath, csv_filepath, 0.5, mod_test, 10)
+pgio.dataframe_to_csv_metadata(save_filepath='./data/', output_dataframe=results, filename='baseline_output_ftrs.csv')
