4a888f396a29af26c72357bd009906dc9d8637c9
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-02-02 11:17:49 +0000

    fix io refactor

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 import pgfinder.matching as matching
-import pgfinder.validation as validation
+import pgfinder.pgio as pgio
 
 mq_filepath = "data/maxquant_test_data.txt"
 csv_filepath = "data/masses/e_coli_monomer_masses.csv"
@@ -11,4 +11,4 @@ results = matching.match(mq_filepath, csv_filepath, 0.5, mod_test, 10)
 print(results.attrs['metadata'])
 print(results)
 
-matching.dataframe_to_csv_metadata(save_filepath='./', output_dataframe=results)
+pgio.dataframe_to_csv_metadata(save_filepath='./', output_dataframe=results)
