d172cdd021dfa4119605a2bb7ef519a3493a7339
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-02-03 08:06:20 +0000

    explicitly specify var set

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -33,7 +33,7 @@ def test_matching_mq_baseline(masses_file_name, mq_file_name, mod_test, mq_basel
     
     results = matching.match(mq_file_name, masses_file_name, 0.5, mod_test, 10)
     
-    output_filepath = pgio.dataframe_to_csv_metadata(tmp_path, output_dataframe=results, filename='output_mq.csv')
+    output_filepath = pgio.dataframe_to_csv_metadata(save_filepath=tmp_path, output_dataframe=results, filename='output_mq.csv')
 
     output_df = pd.read_csv(output_filepath, index_col=0)
 
@@ -44,7 +44,7 @@ def test_matching_ftrs_baseline(masses_file_name, ftrs_file_name, mod_test, ftrs
     
     results = matching.match(ftrs_file_name, masses_file_name, 0.5, mod_test, 10)
     
-    output_filepath = pgio.dataframe_to_csv_metadata(tmp_path, output_dataframe=results, filename='output_ftrs.csv')
+    output_filepath = pgio.dataframe_to_csv_metadata(save_filepath=tmp_path, output_dataframe=results, filename='output_ftrs.csv')
 
     output_df = pd.read_csv(output_filepath, index_col=0)
 
