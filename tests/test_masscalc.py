f19f569c36d3aedb3b15d203f6b8896093d2ebf1
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-02-13 19:48:14 +0000

    format

--- tests/test_masscalc.py
+++ tests/test_masscalc.py
@@ -2,13 +2,15 @@ import pytest
 import pandas as pd
 import pgfinder.masscalc as masscalc
 
-mass_list = pd.read_csv('data/masses/test_monomer_masses.csv')
+mass_list = pd.read_csv("data/masses/test_monomer_masses.csv")
 
-@pytest.fixture(params=list(range(0,mass_list.shape[0])))
+
+@pytest.fixture(params=list(range(0, mass_list.shape[0])))
 def struct_mass(request):
-    return(mass_list.iloc[[request.param]])
+    return mass_list.iloc[[request.param]]
+
 
 def test_mass(struct_mass: pd.DataFrame):
-    Structure = struct_mass['Structure'][1]
-    Monoisotopicmass = struct_mass['Monoisotopicmass'][1]
-    assert(masscalc.mass(Structure)==Monoisotopicmass)
+    Structure = struct_mass["Structure"][1]
+    Monoisotopicmass = struct_mass["Monoisotopicmass"][1]
+    assert masscalc.mass(Structure) == Monoisotopicmass
