e01083f9fda51934a02b4066641d9853be667005
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-02-14 07:33:09 +0000

    include all known masses in tests

--- tests/test_masscalc.py
+++ tests/test_masscalc.py
@@ -2,15 +2,21 @@ import pytest
 import pandas as pd
 import pgfinder.masscalc as masscalc
 
-mass_list = pd.read_csv("data/masses/test_monomer_masses.csv")
+mass_list = pd.concat(
+    [
+        pd.read_csv("data/masses/c_diff_monomer_masses.csv"),
+        pd.read_csv("data/masses/e_coli_monomer_masses.csv"),
+        pd.read_csv("data/masses/test_monomer_masses.csv"),
+    ]
+)
 
 
 @pytest.fixture(params=mass_list["Structure"])
 def struct_mass(request):
-    return(mass_list.loc[mass_list["Structure"] == request.param])
+    return mass_list.loc[mass_list["Structure"] == request.param]
 
 
 def test_mass(struct_mass: pd.DataFrame):
-    Structure = struct_mass["Structure"][1]
-    Monoisotopicmass = struct_mass["Monoisotopicmass"][1]
+    Structure = struct_mass["Structure"].iloc[0]
+    Monoisotopicmass = struct_mass["Monoisotopicmass"].iloc[0]
     assert masscalc.mass(Structure) == Monoisotopicmass
