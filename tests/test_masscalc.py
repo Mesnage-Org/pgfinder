a5e59f92490e6ba2cb284435c879bfb455c2c984
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-02-14 08:01:59 +0000

    layout

--- tests/test_masscalc.py
+++ tests/test_masscalc.py
@@ -19,13 +19,13 @@ def struct_mass(request):
 
 def test_data_integrity():
     # Check all structures in the test set have the same monoisotopic mass.
-    # (There should be the same numbers of duplicate "Structure" as 
-    # duplicate "Structure" AND "Monoisotopicmass".
+    # (There should be the same numbers of duplicate "Structure" as
+    # duplicate "Structure" AND "Monoisotopicmass".)
     mass_group_struct = mass_list.groupby(["Structure"])
     mass_group_struct = mass_group_struct.size().reset_index()
 
     mass_group_all = mass_list.groupby(["Structure", "Monoisotopicmass"])
-    mass_group_all = mass_group_all.size().reset_index().drop('Monoisotopicmass', 1)
+    mass_group_all = mass_group_all.size().reset_index().drop("Monoisotopicmass", 1)
 
     pd.testing.assert_frame_equal(mass_group_struct, mass_group_all)
 
