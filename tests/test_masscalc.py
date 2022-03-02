ef71e6b0d003221853a742518cd4c9cd3f35686a
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-03-02 11:00:26 +0000

    test not needed with smaller test data

--- tests/test_masscalc.py
+++ tests/test_masscalc.py
@@ -23,19 +23,6 @@ def struct_mass(request):
     return mass_list.loc[mass_list["Structure"] == request.param]
 
 
-def test_data_integrity():
-    # Check all structures in the test set have the same monoisotopic mass.
-    # (There should be the same numbers of duplicate "Structure" as
-    # duplicate "Structure" AND "Monoisotopicmass".)
-    mass_group_struct = mass_list.groupby(["Structure"])
-    mass_group_struct = mass_group_struct.size().reset_index()
-
-    mass_group_all = mass_list.groupby(["Structure", "Monoisotopicmass"])
-    mass_group_all = mass_group_all.size().reset_index().drop("Monoisotopicmass", 1)
-
-    pd.testing.assert_frame_equal(mass_group_struct, mass_group_all)
-
-
 def test_mass(caplog, struct_mass: pd.DataFrame, code_masses: pd.DataFrame):
     caplog.set_level(logging.INFO)
     Structure = struct_mass["Structure"].iloc[0]
