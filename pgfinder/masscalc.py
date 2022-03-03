e95c2b991a0e723ed38a95d12be08532be40e91e
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-03-03 09:30:48 +0000

    multiple murnac

--- pgfinder/masscalc.py
+++ pgfinder/masscalc.py
@@ -38,9 +38,24 @@ def mass(structure: str, component_masses: pd.DataFrame) -> float:
         left_on="Code",
         right_on="Code",
     )
+
     logging.info(
         component_masses_filtered.to_string(
             max_rows=component_masses_filtered.shape[0] + 1
         )
     )  # Log dataframe used in mass calc
-    return component_masses_filtered.Monoisotopicmass.sum()
+
+    # Count MurNAc
+    # Take away two hydrogen per "internal" MurNAc (2 x 1.0078Da)
+    n_murnac = int(
+        component_masses_filtered.Structure[
+            component_masses_filtered.Structure.str.contains("MurN")
+        ].count()
+    ) - 1 # One MurNAc is terminal
+
+    murnac_mass_adjust = n_murnac * 2 * 1.0078
+
+    logging.info("Internal MurNAc: " + str(n_murnac))
+    logging.info("Mass adjustment for internal MurNAcs: -" + str(murnac_mass_adjust))
+
+    return component_masses_filtered.Monoisotopicmass.sum() - murnac_mass_adjust
