79a81e67920637ea6b36ed6140ed6cde7807d500
Ankur Patel <ankur.patel@sheffield.ac.uk>
2022-06-29 17:07:26 +0100

    Testing matching_long in data analysis
    long_format_test_data is designed to produce a match with two possible inferred structures (GM-AEJSA and Gm-AEJTG)
    
    Currently clean up method not working as intended

--- pgfinder/Local_Demo.py
+++ pgfinder/Local_Demo.py
@@ -2,7 +2,7 @@ import pgfinder.matching as matching
 import pgfinder.pgio as pgio
 import pgfinder.validation as validation
 
-ftrs_filepath = "data/ftrs_test_data.ftrs"
+ftrs_filepath = "data/long_format_test_data.txt"
 csv_filepath = "data/masses/e_coli_monomer_masses.csv"
 
 masses = pgio.ms_file_reader(ftrs_filepath)
@@ -11,14 +11,10 @@ validation.validate_raw_data_df(masses)
 theo_masses = pgio.theo_masses_reader(csv_filepath)
 validation.validate_theo_masses_df(theo_masses)
 
-mod_test = ['Sodium','Potassium','Anh','DeAc','DeAc_Anh','Nude','Decay','Amidation','Amidase','Double_Anh','multimers_Glyco']
+mod_test = ['Decay']
 
-results = matching.matching_long(masses,theo_masses,10)
+results = matching.data_analysis(masses,theo_masses,0.5,mod_test,10,True)
 
-results2 = matching.matching(masses,theo_masses,10)
-
-# print(results.attrs['metadata'])
-print(results2)
 print(results)
 
 #pgio.dataframe_to_csv_metadata(save_filepath='./', output_dataframe=results)
