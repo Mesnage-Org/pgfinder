43c9b188dedab851396f6fa160d19563e92b679b
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-07-27 11:38:17 +0100

    rm unused cols on input

--- pgfinder/pgio.py
+++ pgfinder/pgio.py
@@ -126,6 +126,10 @@ def ftrs_reader(file: Union[str, Path]) -> pd.DataFrame:
         # print(f"FEATURES : \n")
         # print(f"{ff}")
 
+        ff.drop(columns = ["feature", 
+                           "corrMax", 
+                           "maxIsotopeCount"])
+
         return ff
 
 
