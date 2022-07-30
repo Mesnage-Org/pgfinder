d035158d4569c9401f1728c903c7f9869df5de69
Ankur Patel <ankur.patel@sheffield.ac.uk>
2022-07-30 10:41:43 +0100

    Bench marching performance
    Added timeit functions to time perf diff between iterrows and index based accessing for matching function

--- pgfinder/Local_Demo.py
+++ pgfinder/Local_Demo.py
@@ -5,6 +5,9 @@ from pgfinder import MULTIMERS, MOD_TYPE, MASS_TO_CLEAN
 import pandas as pd
 from decimal import *
 
+import timeit
+import time
+
 ftrs_filepath = "data/ftrs_test_data.ftrs"
 consol_test_data_filepath = "data/long_format_test_data.txt"
 csv_filepath = "data/masses/e_coli_monomer_masses.csv"
@@ -17,14 +20,16 @@ validation.validate_theo_masses_df(theo_masses)
 
 
 
-mod_test = ['Decay']
+# mod_test = ['Decay']
 
-results = matching.data_analysis(masses,theo_masses,0.5,mod_test,10,True)
+# results = matching.data_analysis(masses,theo_masses,0.5,mod_test,10,True)
 
-print(results)
+# print(results)
+def long():
+    matched = matching.matching_long(masses, theo_masses,10)
 
-# matched = matching.matching_long(masses, theo_masses,10)
-
+def short():
+    matched = matching.matching(masses,theo_masses,10)
 # print(matched)
 
 # sugar = Decimal("203.0793")
@@ -35,3 +40,7 @@ print(results)
     
 
 #pgio.dataframe_to_csv_metadata(save_filepath='./', output_dataframe=results)
+
+
+print(timeit.Timer(long).timeit(number=2))
+print(timeit.Timer(short).timeit(number=2))
