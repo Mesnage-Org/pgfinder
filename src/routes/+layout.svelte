678d958a25bb4fed281b2c235cdf09b73da28a17
Brooks J Rady <b.j.rady@gmail.com>
2023-06-22 20:01:25 +0100

    Get PGFinder loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,26 @@
-<script lang='ts'>
+<script lang="ts">
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	import { onMount } from 'svelte';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	onMount(async () => {
+		// Not being able to use Pyodide as a node module is god-awful...
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		let pyodide = await loadPyodide();
+		await pyodide.loadPackage('micropip');
+		const micropip = pyodide.pyimport('micropip');
+		await micropip.install('pgfinder');
+		return pyodide.runPythonAsync(`
+    import pgfinder 
+    print(pgfinder.__version__)
+    `);
+	});
 </script>
 
 <!-- App Shell -->
