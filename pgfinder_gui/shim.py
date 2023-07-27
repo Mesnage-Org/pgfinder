1c8151af42f9af354ba029aee425e87f3fdaec8b
Brooks J Rady <b.j.rady@gmail.com>
2023-07-27 20:57:45 +0100

    Split shim module

--- pgfinder_gui/shim.py
+++ pgfinder_gui/shim.py
@@ -1,19 +1,20 @@
-import io
 import json
-import sys
-import tempfile
-from pathlib import Path, PurePath
-from typing import Dict
+from typing import Dict, List
 
-import pandas as pd
-
-from pgfinder import matching, pgio
-
-MASS_LIB_DIR = Path(sys.modules["pgfinder"].__file__).parent / "masses"
+from pgfinder import matching, pgio, validation
+from pgfinder_gui.internal import (
+    MASS_LIB_DIR,
+    ms_upload_reader,
+    theo_masses_upload_reader,
+)
 
 
 def mass_library_index() -> Dict:
     return json.load(open(MASS_LIB_DIR / "index.json"))
+
+
+def allowed_modifications() -> List:
+    return validation.allowed_modifications()
 
 
 def run_analysis():
@@ -33,58 +34,3 @@ def run_analysis():
         return pgio.dataframe_to_csv_metadata(matched, wide=True)
 
     return {f["name"]: analyze(f) for f in msData.to_py()}
-
-
-def theo_masses_upload_reader(upload: dict) -> pd.DataFrame:
-    """For reading theoretical masses from an interactive jupyter notebook with a file upload widget.
-
-    Parameters
-    ----------
-    upload: dict
-        Dictionary of properties of a file uploaded using
-        `ipywidgets <https://ipywidgets.readthedocs.io/en/stable/examples/Widget%20List.html#File-Upload>`_
-
-    Returns
-    -------
-    pd.DataFrame
-        Pandas Dataframe of theoretical masses.
-    """
-
-    filename = upload["name"]
-    file_contents = open(MASS_LIB_DIR / filename, "rb").read() if upload["content"] is None else upload["content"]
-
-    theo_masses_df = pd.read_csv(io.BytesIO(file_contents))
-    theo_masses_df.columns = ["Inferred structure", "Theo (Da)"]
-    theo_masses_df.attrs["file"] = PurePath(filename).name
-    return theo_masses_df
-
-
-def ms_upload_reader(upload: dict) -> pd.DataFrame:
-    """For reading from an interactive jupyter notebook with a file upload widget.
-
-    Parameters
-    ----------
-    upload: dict
-        Dictionary of properties of a file uploaded using
-       `ipywidgets <https://ipywidgets.readthedocs.io/en/stable/examples/Widget%20List.html#File-Upload>`_
-
-    Returns
-    -------
-    pd.DataFrame
-        Pandas DataFrame of mass information
-    """
-    filename = upload["name"]
-    file_contents = upload["content"]
-    file_temp = tempfile.NamedTemporaryFile(delete=False)
-    file_temp.write(file_contents)
-    file = file_temp.name
-
-    if not filename.find("ftrs") == -1:
-        return_df = pgio.ftrs_reader(file)
-    elif not filename.find("txt") == -1:
-        return_df = pgio.maxquant_file_reader(file)
-    else:
-        raise ValueError("Unknown file type.")
-
-    return_df.attrs["file"] = PurePath(filename).name
-    return return_df
