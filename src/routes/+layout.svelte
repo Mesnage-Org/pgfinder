d3b128006cbaa646c97d0aaeb933ece0d8520649
Brooks J Rady <b.j.rady@gmail.com>
2023-08-28 13:08:11 +0100

    chore(deps): move to Skeleton v2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
-	import { AppShell, AppBar, storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
 <!-- App Shell -->
--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,10 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess()],
 
 	vitePlugin: {
 		inspector: true
