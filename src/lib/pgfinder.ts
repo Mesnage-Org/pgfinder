0d3033ddf50e09ed736fe18ad3dd5a3e93266fbf
Brooks J Rady <b.j.rady@gmail.com>
2023-09-02 16:24:08 +0100

    refactor(pgfinder.ts): flatten init code

--- src/lib/pgfinder.ts
+++ src/lib/pgfinder.ts
@@ -1,41 +1,34 @@
-import { loadPyodide, type PyodideInterface } from 'pyodide';
+import { loadPyodide } from 'pyodide';
 import { defaultPyio } from '$lib/constants';
 
-let pyodide: PyodideInterface;
 const pyio: Pyio = { ...defaultPyio };
 
-// Maybe someday (once top-level await is even more universal), I should get
-// rid of this useless, immediately-called function...
-(async () => {
-	// TODO: Think about a Vite plugin that bundles the node module!
-	// That would mean I don't have to serve things from this indexURL?
-	pyodide = await loadPyodide({
-		indexURL: '/pgfinder-gui/pyodide'
-	});
-	pyodide.registerJsModule('pyio', pyio);
-	await pyodide.loadPackage(['micropip', 'sqlite3']);
-	const micropip = pyodide.pyimport('micropip');
-	await micropip.install('pgfinder');
-	await pyodide.runPythonAsync('import pgfinder; from pgfinder.gui.shim import *');
+const pyodide = await loadPyodide({
+	indexURL: '/pgfinder-gui/pyodide'
+});
+pyodide.registerJsModule('pyio', pyio);
+await pyodide.loadPackage(['micropip', 'sqlite3']);
+const micropip = pyodide.pyimport('micropip');
+await micropip.install('pgfinder');
+await pyodide.runPythonAsync('import pgfinder; from pgfinder.gui.shim import *');
 
-	const pgfinderVersion = await pyodide.runPythonAsync('pgfinder.__version__');
+const pgfinderVersion = await pyodide.runPythonAsync('pgfinder.__version__');
 
-	const proxy = await pyodide.runPythonAsync('allowed_modifications()');
-	const allowedModifications = proxy.toJs();
-	proxy.destroy();
+const proxy = await pyodide.runPythonAsync('allowed_modifications()');
+const allowedModifications = proxy.toJs();
+proxy.destroy();
 
-	const json = await pyodide.runPythonAsync('mass_library_index()');
-	const massLibraries = JSON.parse(json);
+const json = await pyodide.runPythonAsync('mass_library_index()');
+const massLibraries = JSON.parse(json);
 
-	postMessage({
-		type: 'Ready',
-		content: {
-			pgfinderVersion,
-			allowedModifications,
-			massLibraries
-		}
-	});
-})();
+postMessage({
+	type: 'Ready',
+	content: {
+		pgfinderVersion,
+		allowedModifications,
+		massLibraries
+	}
+});
 
 onmessage = async ({ data }) => {
 	Object.assign(pyio, data);
