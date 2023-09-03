0328356dc8752841383a1a7bcb9d8f46562877cb
Brooks J Rady <b.j.rady@gmail.com>
2023-09-03 17:54:30 +0100

    revert(pgfinder.ts): added back async arrow function

--- src/lib/pgfinder.ts
+++ src/lib/pgfinder.ts
@@ -1,35 +1,40 @@
 import type { PyProxy, PythonError } from 'pyodide/ffi';
-import { loadPyodide } from 'pyodide';
+import { loadPyodide, type PyodideInterface } from 'pyodide';
 import { defaultPyio } from '$lib/constants';
 
 const pyio: Pyio = { ...defaultPyio };
-const pyodide = await loadPyodide({
-	indexURL: '/pgfinder-gui/pyodide'
-});
+let pyodide: PyodideInterface;
 
-pyodide.registerJsModule('pyio', pyio);
-await pyodide.loadPackage(['micropip', 'sqlite3']);
-const micropip = pyodide.pyimport('micropip');
-await micropip.install('pgfinder');
-await pyodide.runPythonAsync('import pgfinder; from pgfinder.gui.shim import *');
+// Maybe someday (once top-level await is even more universal), I should get
+// rid of this useless, immediately-called function...
+(async () => {
+	pyodide = await loadPyodide({
+		indexURL: '/pgfinder-gui/pyodide'
+	});
+	pyodide.registerJsModule('pyio', pyio);
+	await pyodide.loadPackage(['micropip', 'sqlite3']);
+	const micropip = pyodide.pyimport('micropip');
+	await micropip.install('pgfinder');
+	await pyodide.runPythonAsync('import pgfinder; from pgfinder.gui.shim import *');
 
-const pgfinderVersion = await pyodide.runPythonAsync('pgfinder.__version__');
+	const pgfinderVersion = await pyodide.runPythonAsync('pgfinder.__version__');
 
-const proxy = await pyodide.runPythonAsync('allowed_modifications()');
-const allowedModifications = proxy.toJs();
-proxy.destroy();
+	const proxy = await pyodide.runPythonAsync('allowed_modifications()');
+	const allowedModifications = proxy.toJs();
+	proxy.destroy();
 
-const json = await pyodide.runPythonAsync('mass_library_index()');
-const massLibraries = JSON.parse(json);
+	const json = await pyodide.runPythonAsync('mass_library_index()');
+	const massLibraries = JSON.parse(json);
 
-postMessage({
-	type: 'Ready',
-	content: {
-		pgfinderVersion,
-		allowedModifications,
-		massLibraries
-	}
-});
+	postMessage({
+		type: 'Ready',
+		content: {
+			pgfinderVersion,
+			allowedModifications,
+			massLibraries
+		}
+	});
+})();
 
 function postResult(proxy: PyProxy) {
 	const csvFiles = proxy.toJs();
