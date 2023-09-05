a04e331e70b8571f48d755b8d8095e1c6862275b
Brooks J Rady <b.j.rady@gmail.com>
2023-09-06 00:04:51 +0100

    ci(tests): simplify playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { type PlaywrightTestConfig, devices } from '@playwright/test';
+import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
@@ -6,31 +6,7 @@ const config: PlaywrightTestConfig = {
 		port: 4173
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
-	projects: [
-		/* Test against desktop browsers */
-		{
-			name: 'chromium',
-			use: { ...devices['Desktop Chrome'] }
-		},
-		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] }
-		},
-		{
-			name: 'webkit',
-			use: { ...devices['Desktop Safari'] }
-		},
-		/* Test against mobile viewports. */
-		{
-			name: 'Mobile Chrome',
-			use: { ...devices['Pixel 5'] }
-		},
-		{
-			name: 'Mobile Safari',
-			use: { ...devices['iPhone 12'] }
-		}
-	]
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/
 };
 
 export default config;
