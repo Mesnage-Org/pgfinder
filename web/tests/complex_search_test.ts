12f2106437c883363dd9858d9cfb63193c60a4f7
Brooks J Rady <b.j.rady@gmail.com>
2023-10-15 20:36:20 +0100

    fix(tests): update e2e tests with new modification names

--- web/tests/complex_search_test.ts
+++ web/tests/complex_search_test.ts
@@ -9,7 +9,8 @@ test('run a complex search', async ({ page }) => {
 	await page.getByRole('option', { name: 'Complex' }).click();
 
 	await page.getByRole('button', { name: 'Advanced Options' }).click();
-	await page.getByRole('option', { name: 'Cross-Linked Multimers (=)' }).click();
+	await page.getByRole('option', { name: '3-3 and 3-4 Cross-Linked Multimers (=)' }).click();
+	await page.getByRole('option', { name: '3-1 Cross-Linked Multimers (=)' }).click();
 	await page.getByRole('option', { name: 'Glycosidic Multimers (-)' }).click();
 	await page.getByRole('option', { name: 'Lactyl Multimers (=Lac)' }).click();
 	await page.getByRole('option', { name: 'Anhydro-MurNAc (Anh)' }).click();
