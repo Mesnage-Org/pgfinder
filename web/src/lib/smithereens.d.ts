92d87457f8048ad23830ed1bad56faad0990ecc8
slackline <nshephard@protonmail.com>
2024-03-20 19:23:39 +0000

    feat: Rust WebAssmebly components for fragment builder

---
+++ web/src/lib/smithereens.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {Peptidoglycan} precursor
+* @returns {string}
+*/
+export function pg_to_fragments(precursor: Peptidoglycan): string;
+/**
+*/
+export class Peptidoglycan {
+  free(): void;
+/**
+* @param {string} structure
+*/
+  constructor(structure: string);
+/**
+* @returns {string}
+*/
+  monoisotopic_mass(): string;
+}
