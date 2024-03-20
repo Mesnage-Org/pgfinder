5687b33ca09e5f1b0a94b63741a1ceb6218c2c5c
Brooks J Rady <b.j.rady@gmail.com>
2024-03-20 13:50:05 -0700

    tests(playwright): give slow tests a bit more time

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -6,7 +6,8 @@ const config: PlaywrightTestConfig = {
 		port: 4173
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	timeout: 5 * 60 * 1000
 };
 
 export default config;
