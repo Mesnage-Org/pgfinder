5556e168997be693a8832f2fd075375ba47060f9
Brooks J Rady <b.j.rady@gmail.com>
2025-05-15 16:00:27 +0100

    chore(deps): remove postcss and add vite plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,10 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import wasmPack from "vite-plugin-wasm-pack";
 
 export default defineConfig({
-  plugins: [sveltekit(), wasmPack("./smithereens")],
+  plugins: [tailwindcss(), sveltekit(), wasmPack("./smithereens")],
   define: {
     WEBUI_VERSION: JSON.stringify(process.env.npm_package_version),
   },
